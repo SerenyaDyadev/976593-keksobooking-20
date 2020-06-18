bc50624ba600475b4c75f066adda8c4d060d71cb
Serenya Dyadev <3684182@gmail.com>
2020-06-18 12:01:23 +0300

    разбивка на модули

---
+++ js/card.js
@@ -0,0 +1,58 @@
+'use strict';
+
+(function () {
+
+  var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+  var fragmentPhoto = document.createDocumentFragment();
+  var fragmentWithFeatures = document.createDocumentFragment();
+
+  var accommodationTypes = {
+    flat: 'Квартира',
+    bungalo: 'Бунгало',
+    house: 'Дом',
+    palace: 'Дворец'
+  };
+
+  var getPhotoSrc = function (photos) {
+    for (var i = 0; i < photos.length; i++) {
+      var photoElement = cardTemplate.querySelector('.popup__photo').cloneNode(true);
+      photoElement.src = photos[i];
+      fragmentPhoto.appendChild(photoElement);
+    }
+
+    return fragmentPhoto;
+  };
+
+  var getFeatures = function (features) {
+    var listFeauters = cardTemplate.querySelector('.popup__features').cloneNode(false);
+
+    for (var i = 0; i < features.length; i++) {
+      var listElement = document.createElement('li');
+      listElement.className = 'popup__feature popup__feature--' + features[i];
+      listFeauters.appendChild(listElement);
+    }
+
+    fragmentWithFeatures.appendChild(listFeauters);
+
+    return fragmentWithFeatures;
+  };
+
+  var getCard = function (data) {
+    var cardElement = cardTemplate.cloneNode(true);
+    cardElement.querySelector('.popup__avatar').src = data.author.avatar;
+    cardElement.querySelector('.popup__title').textContent = data.offer.title;
+    cardElement.querySelector('.popup__text--address').textContent = data.offer.address;
+    cardElement.querySelector('.popup__text--price').textContent = data.offer.price + '₽/ночь';
+    cardElement.querySelector('.popup__type').textContent = accommodationTypes[data.offer.type];
+    cardElement.querySelector('.popup__text--capacity').textContent = data.offer.rooms + ' комнаты для ' + data.offer.guests + ' гостей';
+    cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + data.offer.checkin + ', выезд до  ' + data.offer.checkout;
+    cardElement.removeChild(cardElement.querySelector('.popup__features'));
+    cardElement.insertBefore(getFeatures(data.offer.features), cardElement.querySelector('.popup__description'));
+    cardElement.querySelector('.popup__description').textContent = data.offer.description;
+    cardElement.querySelector('.popup__photos').replaceChild(getPhotoSrc(data.offer.photos), cardElement.querySelector('.popup__photo'));
+
+    return cardElement;
+  };
+
+  window.card = getCard;
+})();
---
+++ js/form.js
@@ -0,0 +1,92 @@
+'use strict';
+(function () {
+  window.form = function () {
+    // var forms = document.querySelector('.ad-form');
+    var addForm = document.querySelector('.ad-form');
+    var mapFilters = document.querySelector('.map__filters');
+
+    var addEnableDisabledAttribute = function (elements, boolean) {
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].disabled = boolean;
+      }
+    };
+
+    addEnableDisabledAttribute(addForm.querySelectorAll('select, fieldset'), true);
+    addEnableDisabledAttribute(mapFilters.querySelectorAll('select, fieldset'), true);
+
+    var capacityRooms = addForm.querySelector('#room_number');
+    var capacityGuests = addForm.querySelector('#capacity');
+
+    var optionsEnable = function () {
+      for (var j = capacityGuests.selectedIndex; j < capacityGuests.length - 1; j++) {
+        capacityGuests.options[j].disabled = false;
+      }
+    };
+
+    var validationRoomsGuests = function () {
+      addEnableDisabledAttribute(capacityGuests, true);
+
+      if (capacityRooms[capacityRooms.selectedIndex].value === '100') {
+        capacityGuests.options[capacityGuests.length - 1].selected = true;
+        capacityGuests.options[capacityGuests.length - 1].disabled = false;
+      } else {
+        for (var i = 0; i < capacityGuests.length; i++) {
+          if (capacityRooms[capacityRooms.selectedIndex].value === capacityGuests.options[i].value) {
+            capacityGuests.options[i].selected = true;
+            optionsEnable();
+          }
+        }
+      }
+    };
+
+    var typePlace = addForm.querySelector('#type');
+    var pricePlace = addForm.querySelector('#price');
+
+    var typesAndPrices = {
+      bungalo: 0,
+      flat: 1000,
+      house: 5000,
+      palace: 10000
+    };
+
+    var validationTypePrice = function () {
+      pricePlace.min = typesAndPrices[typePlace.options[typePlace.selectedIndex].value];
+      pricePlace.placeholder = typesAndPrices[typePlace.options[typePlace.selectedIndex].value];
+    };
+
+    var timeCheckin = addForm.querySelector('#timein');
+    var timeCheckout = addForm.querySelector('#timeout');
+
+    var validationTimeCheckout = function () {
+      timeCheckout.options[timeCheckin.selectedIndex].selected = true;
+    };
+
+    var validationTimeCheckin = function () {
+      timeCheckin.options[timeCheckout.selectedIndex].selected = true;
+    };
+
+    capacityRooms.addEventListener('change', function () {
+      validationRoomsGuests();
+    });
+
+    typePlace.addEventListener('change', function () {
+      validationTypePrice();
+    });
+
+    timeCheckin.addEventListener('change', function () {
+      validationTimeCheckout();
+    });
+
+    timeCheckout.addEventListener('change', function () {
+      validationTimeCheckin();
+    });
+
+    addForm.classList.remove('ad-form--disabled');
+    addEnableDisabledAttribute(addForm.querySelectorAll('select, fieldset'), false);
+    addEnableDisabledAttribute(mapFilters.querySelectorAll('select, fieldset'), false);
+    validationRoomsGuests();
+    validationTypePrice();
+    validationTimeCheckout();
+    validationTimeCheckin();
+  };
+})();
