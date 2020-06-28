1f1bf8cee2d8c939a71bd0df1b70ad1d7ef20055
Serenya Dyadev <3684182@gmail.com>
2020-06-28 17:19:42 +0300

    исправление замечаний 2

--- js/main.js
+++ js/main.js
@@ -10,26 +10,31 @@ var mapPinLocationX = mapCircleCenterX;
 var mapPinLocationY = +(mapPinMain.style.top).split('px')[0] + MAP_PIN_HEIGHT;
 var inputs = document.querySelectorAll('select, fieldset');
 var successMessage = document.querySelector('#success').content.querySelector('.success');
-
-
-for (var i = 0; i < inputs.length; i++) {
-  inputs[i].disabled = true;
-}
-
-var inputAddress = document.querySelector('#address');
-inputAddress.value = mapCircleCenterX + ', ' + mapCircleCenterY;
-
 var arrayData;
+
+var disableInputs = function () {
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].disabled = true;
+  }
+};
+
+disableInputs();
+
+var addressInputValue = function (x, y) {
+  document.querySelector('#address').value = x + ', ' + y;
+};
+
+addressInputValue(mapCircleCenterX, mapCircleCenterY);
 
 var onLoadData = function (data) {
 
-  for (i = 0; i < data.length; i++) {
+  for (var i = 0; i < data.length; i++) {
     fragmentMapPin.appendChild(window.pin(data[i]));
   }
 
   window.map.appendChild(fragmentMapPin);
   window.map.classList.remove('map--faded');
-  inputAddress.value = mapPinLocationX + ', ' + mapPinLocationY;
+  addressInputValue(mapPinLocationX, mapPinLocationY);
   window.form();
 
   arrayData = data;
@@ -58,7 +63,9 @@ mapPinMain.addEventListener('keydown', function (evt) {
 var adForm = document.querySelector('.ad-form');
 adForm.addEventListener('submit', function (evt) {
   window.load('https://javascript.pages.academy/keksobooking', 'POST', function () {
+    disableInputs();
     window.clear();
+    addressInputValue(mapCircleCenterX, mapCircleCenterY);
     window.effect(successMessage);
   }, new FormData(adForm));
   evt.preventDefault();
@@ -67,6 +74,8 @@ adForm.addEventListener('submit', function (evt) {
 
 var formResetButton = adForm.querySelector('.ad-form__reset');
 formResetButton.addEventListener('click', function () {
+  disableInputs();
   window.clear();
+  addressInputValue(mapCircleCenterX, mapCircleCenterY);
 });
 
