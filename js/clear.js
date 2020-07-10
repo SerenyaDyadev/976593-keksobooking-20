6ac9f38c64d0c2703263c868f41a21bff820904e
Serenya Dyadev <3684182@gmail.com>
2020-07-10 08:23:25 +0300

    placeholder

--- js/clear.js
+++ js/clear.js
@@ -3,6 +3,7 @@
   var mapPinMain = document.querySelector('.map__pin--main');
   var filterForm = document.querySelector('.map__filters');
   var adForm = document.querySelector('.ad-form');
+  var placeholderInputPriceDefault = adForm.querySelector('#price').placeholder;
   var pinCircleDefaultLocalX = mapPinMain.style.left;
   var pinCircleDefaultLocalY = mapPinMain.style.top;
   var avatarPhotoDefault = document.querySelector('.ad-form-header__preview').querySelector('img');
@@ -10,13 +11,14 @@
   var previewPhoto = document.querySelector('.ad-form__photo');
 
   window.clear = function () {
-    var previewPhotoImages = previewPhoto.querySelectorAll('img');
+    adForm.querySelector('#price').placeholder = placeholderInputPriceDefault;
 
     window.utils.closePopupCard();
     window.utils.deletePins();
 
     avatarPhotoDefault.src = avatarPhotoDefaultSrc;
 
+    var previewPhotoImages = previewPhoto.querySelectorAll('img');
     if (previewPhotoImages.length > 0) {
       previewPhotoImages.forEach(function (previewPhotoImg) {
         previewPhotoImg.parentNode.removeChild(previewPhotoImg);
